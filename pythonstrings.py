c644b73271aff32da776b20ecabc959c05dac48d
Kirk Abbott <kirkabbott1@gmail.com>
2016-09-26 14:22:29 -0400

    adds updated python practice

--- pythonstrings.py
+++ pythonstrings.py
@@ -17,11 +17,29 @@
 #
 # x = [1,2,3,4,5,6,7]
 # print sum(x)
+#
+# sorted_list = [1,2,3,4,5,6,7,22,23,24]
+# odd_numbers = []
+#
+# for i in sorted_list:
+#     if i%2 != 0:
+#         odd_numbers.append(i)
+# print 'highest number is ', max(odd_numbers)
 
-sorted_list = [1,2,3,4,5,6,7,22,23,24]
-odd_numbers = []
+list1 = [1,23,35,5,6,32,34,36]
+# listeven = []
+# print min(list1)
+# for i in list1:
+#     if i%2 == 0:
+#         listeven.append(i)
+# #     print listeven
+# listpos = []
+# for i in list1:
+#     if i > 0:
+#         listpos.append(i)
+# print listpos
 
-for i in sorted_list:
-    if i%2 != 0:
-        odd_numbers.append(i)
-print 'highest number is ', max(odd_numbers)
+factorlist = []
+for i in list1:
+    factorlist.append(i * 2)
+print factorlist
